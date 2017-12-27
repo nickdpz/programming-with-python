9dda7edfd3c6447f832ee6435457abf8e6f027b2
Nicolas Pastram <34488552+nickdpz@users.noreply.github.com>
2017-12-27 15:51:20 -0500

    Graficas Funcionales
    Se realiza el avance de las graficas funcionales en python

---
+++ Campo_Electrico.py
@@ -0,0 +1,69 @@
+#Nicolas Pastran cod:20151005087
+#Katherin Castelblanco cod:20151005571
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as sc
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+fig, ax = plt.subplots(figsize=(20,10))
+plt.subplots_adjust(left=0.15, bottom=0.30)
+po=0.000002
+a=20
+x = np.arange(0.01,40,0.01)
+
+H1 = lambda x: np.piecewise(x,[x < 20. , x >= 20],[1, 0])
+H2 = lambda x: np.piecewise(x,[x < 20. , x >= 20],[0, 1])   
+
+K= H1(x)*(po/sc.epsilon_0)*((x/3)-(x**3)/(5*a**2))+H2(x)*((a**3)/x**2)*(po/sc.epsilon_0)*(0.133333333)
+k1=H2(x)*((a**3)/x**2)*(po/sc.epsilon_0)*(0.133333333)
+
+plt.plot(x,K,'blue',linewidth=2,label="Campo Electrico Interno",color="blue")
+
+plt.plot(x,k1, linewidth=2, label="Campo Electrico Externo",color="green")
+plt.legend(loc=1)
+
+font1 = {'family' : 'century',
+        'color'  : 'black',
+        'weight' : 'normal',
+        'size'   : 1,
+        }
+
+font2 = {'family' : 'century',
+        'color'  : 'black',
+        'weight' : 'normal',
+        'size'   : 1,
+        }
+
+font3 = {'family' : 'century',
+        'color'  : 'black',
+        'weight' : 'normal',
+        'size'   : 18,
+        }
+plt.title(r'$Campo$ $Electrico$ $ en$ $dentro$  $y$ $fuera$ $de$ $una$ $esfera$',fontdict=font3)
+plt.xlabel(r'$Radio(m)$', fontdict=font3)
+plt.ylabel(r'$Campo$ $Electrico(N/c)$', fontdict=font3)
+plt.grid(True)
+
+
+point, = plt.plot(x[1],K[1],"red",marker='o')
+
+
+
+axcolor = 'black'
+axfreq = plt.axes([0.15, 0.1, 0.7, 0.03], axisbg=axcolor)
+sfreq = Slider(axfreq, 'Radio', 1, 4000, valinit=4)
+msg=''
+
+def update(val):
+    freq = sfreq.val
+    point.set_ydata(K[freq])
+    point.set_xdata(x[freq])
+    msg=r'$Radio=%f ,$  $ Potencial=%f$'%(x[freq],K[freq])
+    fig.canvas.draw_idle()
+    plt.legend([point], [msg],bbox_to_anchor=(1, 1),
+           bbox_transform=plt.gcf().transFigure)
+sfreq.on_changed(update)
+
+
+plt.show()
---
+++ Figura_14_10.py
@@ -0,0 +1,34 @@
+﻿#Figura 14.10
+#Katherin Castelblanco - 20151005571
+#Nicolas Pastran       - 20151005087
+
+from mpl_toolkits.mplot3d import Axes3D
+import scipy.constants as constant
+import matplotlib.pyplot as plot
+import numpy as np
+from matplotlib import cm
+import matplotlib.animation as animation
+
+radio, theta = np.meshgrid(np.linspace(0.35, 10, 120),np.linspace(0, 2*np.pi, 100))
+x = radio*np.cos(theta)
+y = radio*np.sin(theta)
+
+fig=plot.figure('Figura 14.10',figsize=(20,10))
+
+def update(t):
+	plot.cla()	
+        ax = fig.add_subplot(111, projection='3d')
+        plot.title('K$\lambda$ Por Nicolas Pastran y Katherin Castelblanco',fontsize = 30, color = 'black', verticalalignment = 'baseline', horizontalalignment = 'center')
+        z = (np.sin(theta+constant.pi/2)**2)*(1/(radio**2)+1)*np.cos((0.8*t)-radio+np.arctan(radio));
+        frame=ax.plot_surface(x,y,z, rstride=1, cstride=1, cmap='hot', vmin=-1.4, vmax=1.4, linewidth=.2)
+        ax.set_zlim(-7, 7)
+        ax.set_ylim(-7, 7)
+        ax.set_xlim(-10, 10)
+        ax.set_xlabel('x',fontsize=20)
+        ax.set_ylabel('y',fontsize=20)
+        ax.set_zlabel('z',fontsize=20)
+        return frame,
+
+animation=animation.FuncAnimation(fig, update, frames=180, interval=50)
+
+plot.show()
